be1887d80f1a5ed55822fcca2ad6ccfd04ae8a42
kotahorii <khorii.micron@gmail.com>
2022-01-05 19:08:25 +0900

    [Update] auth test

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
+  let(:json) { JSON.parse(response.body) }
   describe 'ユーザーログインのテスト' do
     let!(:user) { create(:user) }
     context 'パラメータが正常な場合' do
@@ -13,7 +14,6 @@ RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
       end
 
       it 'レスポンスが正しいこと' do
-        json = JSON.parse(response.body)
         expect(json['data']['email']).to eq(user['email'])
       end
     end
@@ -28,7 +28,6 @@ RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
       end
 
       it 'jsonがnilでかえされること' do
-        json = JSON.parse(response.body)
         expect(json['user']).to eq nil
       end
     end
@@ -47,7 +46,6 @@ RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
 
       it 'レスポンスのjsonが正しいこと' do
         post api_v1_user_registration_path, params: params
-        json = JSON.parse(response.body)
         expect(json['data']['name']).to eq 'test'
       end
     end
@@ -80,6 +78,40 @@ RSpec.describe Api::V1::Auth::RegistrationsController, type: :request do
         delete destroy_api_v1_user_session_path, headers: headers
         expect(response.status).to eq(200)
         expect(api_v1_current_user.reload.tokens).to be_blank
+      end
+    end
+  end
+
+  describe 'ログイン中のユーザーのデータ取得テスト' do
+    context 'ユーザーがログインしているとき' do
+      let!(:api_v1_current_user) { create(:user) }
+      let(:headers) { api_v1_current_user.create_new_auth_token }
+      context 'パラメータが正常な場合' do
+        before do
+          get api_v1_auth_sessions_path, headers: headers
+        end
+
+        it 'HTTPステータスが200であること' do
+          expect(response.status).to eq(200)
+        end
+
+        it 'jsonで正しい投稿が取得されること' do
+          expect(json['name']).to eq 'rspec-user'
+        end
+      end
+
+      context 'パラメータが正常な場合' do
+        before do
+          get api_v1_auth_sessions_path
+        end
+
+        it 'HTTPステータスが422であること' do
+          expect(response.status).to eq(422)
+        end
+
+        it 'jsonがnilでかえされること' do
+          expect(json['name']).to eq nil
+        end
       end
     end
   end
